3998f25c2e5d4864e2d94d1769bdba049d1900ba
afrizalfikri22 <l200230195@student.ums.ac.id>
2025-12-06 18:53:05 +0700

    feat(auth): implement custom user model with roles and admin config

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('username', 'email', 'role', 'nim')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Fields', {'fields': ('role', 'nim')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Extra Fields', {'fields': ('role', 'nim')}),
+    )
+    
+admin.site.register(User, CustomUserAdmin)
--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('admin', 'Admin'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
+    nim = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return f"{self.username} - {self.role}"
