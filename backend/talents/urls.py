4e676ba39b4e19314d625da14fbe43c9cc762172
aditya <l200230185@student.ums.ac.id>
2025-12-08 13:36:34 +0700

    feat(views & urls): add CV download endpoint dan route

--- backend/talents/urls.py
+++ backend/talents/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PublicTalentListView, PublicTalentDetailView, MyProfileView
+from .views import PublicTalentListView, PublicTalentDetailView, MyProfileView, DownloadCVView
 
 urlpatterns = [
     # 1. URL untuk melihat semua talent (Public)
@@ -9,6 +9,10 @@ urlpatterns = [
     # 2. URL untuk edit profil sendiri (Private - Harus Login)
     # Akses: /api/talents/me/
     path('me/', MyProfileView.as_view(), name='my-profile'),
+
+    # 4. URL untuk download CV (Public)
+    # Akses: /api/talents/<username>/download-cv/
+    path('<str:user__username>/download-cv/', DownloadCVView.as_view(), name='download-cv'),
 
     # 3. URL untuk melihat detail talent berdasarkan username (Public)
     # Akses: /api/talents/afrizal/
