44671973204464fa7651dae15e659b7b4209085f
Narendra Satya Adikurniawan <narendrasatyaadi7@gmail.com>
2025-12-08 12:11:35 +0700

    feat: logo, navbar, footer, tailwind, view app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={
+              <div className="max-w-7xl mx-auto px-4 py-16">
+                <h1 className="text-4xl font-bold text-center text-slate-900 mb-4">
+                  Selamat Datang di MyTalenta
+                </h1>
+                <p className="text-center text-gray-600">
+                  Platform untuk menampilkan talenta Mahasiswa UMS
+                </p>
+              </div>
+            } />
+            <Route path="/login" element={
+              <div className="max-w-7xl mx-auto px-4 py-16">
+                <h1 className="text-3xl font-bold text-gray-900">
+                </h1>
+              </div>
+            } />
+          </Routes>
+        </main>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
