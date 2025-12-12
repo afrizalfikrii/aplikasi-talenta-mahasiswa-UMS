4a1beb18d5afd79495b50096ef1e9ed34a8769f7
unknown <l200230175@student.ums.ac.id>
2025-12-12 14:37:07 +0700

    feat: link a cardHome to talenta detail

--- frontend/src/component/layout/CardHome.tsx
+++ frontend/src/component/layout/CardHome.tsx
@@ -1,4 +1,4 @@
-// data dummy
+import { Link } from "react-router-dom"; 
 import { useState, useEffect } from "react";
 import { getPublicTalents } from "../../services/talent.service";
 import type { Talent } from "../../types/talent";
@@ -30,7 +30,14 @@ export default function CardHome() {
 
   return (
     <>
+    
     {talents?.map((talenta) => (
+    <Link
+      to={`/talenta/${talenta.username}`}
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      key={talenta.id}
+      className="block hover:scale-[1.02] transition-transform"
+    >
     <div key={talenta.id} className="bg-white rounded-lg p-4 hover:shadow-md transition-shadow duration-300 text-center border border-slate-200">
       <div className="w-20 h-20 mx-auto mb-3 bg-slate-100 rounded-full flex items-center justify-center">
         <img src={getAvatar(talenta.profile_picture)} alt={`${talenta.username} profile`} className="w-full h-full rounded-full object-cover" />
@@ -52,6 +59,7 @@ export default function CardHome() {
         
       </div>
     </div>
+    </Link>
     ))}
     </>
     );
