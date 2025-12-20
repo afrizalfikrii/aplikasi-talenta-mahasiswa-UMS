cc37848fc109827b4d9d8e11fb61caba7a905533
Narendra Satya Adikurniawan <narendrasatyaadi7@gmail.com>
2025-12-20 22:21:05 +0700

    feat: table head

--- frontend/src/features/admin/components/TableHeadTalenta.tsx
+++ frontend/src/features/admin/components/TableHeadTalenta.tsx
@@ -5,13 +5,13 @@ const TableHeadTalenta = () => {
     
     <thead className="border-b dark:border-slate-700 text-gray-600 dark:text-gray-400">
       <tr>
-        <th className="py-3 text-left">NIM</th>
-        <th className="py-3 text-left">Nama</th>
-        <th className="py-3 text-left">Program Studi</th>
-        <th className="py-3 text-left">Email</th>
-        <th className="py-3 text-left">Skills</th>
-        <th className="py-3 text-left">Status</th>
-        <th className="py-3 text-center">Aksi</th>
+        <th className="py-3 text-left w-32 whitespace-nowrap">NIM</th>
+        <th className="py-3 text-left w-64">Nama</th>
+        <th className="py-3 text-left w-44 whitespace-nowrap">Program Studi</th>
+        <th className="py-3 text-left w-72">Email</th>
+        <th className="py-3 text-left w-64">Skills</th>
+        <th className="py-3 text-left w-28">Status</th>
+        <th className="py-3 text-center w-24">Aksi</th>
       </tr>
     </thead>
     // <DaftarTalentaBody />
