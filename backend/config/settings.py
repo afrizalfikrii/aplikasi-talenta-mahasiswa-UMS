4e5982f77ab17044b8fb000b5ca46a1e5c2a024c
Afrizal Fikri <afrizalfikri91@gmail.com>
2026-01-28 12:59:57 +0700

    fix: Hardcode ALLOWED_HOSTS to wildcard for debug

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else []
+ALLOWED_HOSTS = ["*"]  # ✅ Force Allow All untuk debugging deployment
 
 
 # Application definition
